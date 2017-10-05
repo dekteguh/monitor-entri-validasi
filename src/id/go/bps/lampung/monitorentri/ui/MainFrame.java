7a352d5274ff342c6bb5802c631dc6f9834afbeb
Eko Teguh Widodo <dekteguh@gmail.com>
2017-10-06 03:03:40 +0700

    Perbaikan MainFrame, munculnya menu jika yang login admin,supervisor,operator

--- src/id/go/bps/lampung/monitorentri/ui/MainFrame.java
+++ src/id/go/bps/lampung/monitorentri/ui/MainFrame.java
@@ -11,8 +11,9 @@ package id.go.bps.lampung.monitorentri.ui;
  */
 public class MainFrame extends javax.swing.JFrame {
 
-    String username, sensusSurveiId;
-    boolean isLoggedIn = true;
+    int status = 0;
+    String sensusSurveiId;
+    boolean isLoggedIn = false;
     /**
      * Creates new form MainFrame
      */
@@ -22,23 +23,64 @@ public class MainFrame extends javax.swing.JFrame {
         MainFrame.this.setSize(1024, 720);
         MainFrame.this.setLocationRelativeTo(null);
         MainFrame.this.setResizable(false);
-        MainFrame.this.checkLogin(isLoggedIn);
+        MainFrame.this.setLogged(this.isLoggedIn, 0, "");
     }
     
-    public void setLogged(boolean isLoggedIn, String username, String sensusSurvei){
+    public void setLogged(boolean isLoggedIn, int status, String sensusSurvei){
         this.isLoggedIn = isLoggedIn;
-        this.username = username;
+        this.status = status;
         this.sensusSurveiId = sensusSurvei;
+        
+        checkLogin(this.isLoggedIn);
+        this.setMenuEnable(isLoggedIn, status);
     }
     
     public void checkLogin(boolean b){
         if(b){
-            //MainFrame.this.mainPanel.remove(new LoginPanel(this));
-            MainFrame.this.mainPanel.add(new EntrianPanel(this));
+            this.mainPanel.removeAll();
+            this.mainPanel.add(new HomePanel(this));
+            this.mainPanel.repaint();
+            this.mainPanel.revalidate();
         }else{
-            //MainFrame.this.mainPanel.remove(new EntrianPanel(this));
-            MainFrame.this.mainPanel.add(new LoginPanel(this));
+            this.mainPanel.removeAll();
+            this.mainPanel.add(new LoginPanel(this));
+            this.mainPanel.repaint();
+            this.mainPanel.revalidate();
         }
+    }
+    
+    private void setMenuEnable(boolean b, int status){
+        switch (status) {
+            case 1:
+                //admin
+                this.btnMenuHome.setVisible(b);
+                this.btnMenuEntri.setVisible(b);
+                this.btnMenuValidasi.setVisible(b);
+                this.btnMenuProgress.setVisible(b);
+                this.btnMenuLogout.setVisible(b);
+                break;
+            case 2:
+                //supervisor
+                this.btnMenuHome.setVisible(b);
+                this.btnMenuProgress.setVisible(b);
+                this.btnMenuLogout.setVisible(b);
+                break;
+            case 3:
+                //operator
+                this.btnMenuHome.setVisible(b);
+                this.btnMenuEntri.setVisible(b);
+                this.btnMenuValidasi.setVisible(b);
+                this.btnMenuLogout.setVisible(b);
+                break;
+            case 0:
+                this.btnMenuHome.setVisible(b);
+                this.btnMenuEntri.setVisible(b);
+                this.btnMenuValidasi.setVisible(b);
+                this.btnMenuProgress.setVisible(b);
+                this.btnMenuLogout.setVisible(b);
+                break;
+        }
+        
     }
 
     /**
@@ -52,57 +94,89 @@ public class MainFrame extends javax.swing.JFrame {
 
         mainPanel = new javax.swing.JPanel();
         headerPanel = new javax.swing.JPanel();
-        menuEntri = new javax.swing.JLabel();
-        menuValidasi = new javax.swing.JLabel();
-        jLabel3 = new javax.swing.JLabel();
-        menuProgress = new javax.swing.JLabel();
+        btnMenuEntri = new javax.swing.JButton();
+        btnMenuValidasi = new javax.swing.JButton();
+        btnMenuProgress = new javax.swing.JButton();
+        btnMenuLogout = new javax.swing.JButton();
+        btnMenuHome = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         mainPanel.setPreferredSize(new java.awt.Dimension(1024, 670));
-        mainPanel.setLayout(new java.awt.BorderLayout());
+        mainPanel.setLayout(new java.awt.CardLayout());
+        getContentPane().add(mainPanel, java.awt.BorderLayout.CENTER);
 
         headerPanel.setBackground(new java.awt.Color(0, 102, 102));
         headerPanel.setPreferredSize(new java.awt.Dimension(1024, 54));
 
-        menuEntri.setBackground(new java.awt.Color(0, 102, 255));
-        menuEntri.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        menuEntri.setForeground(new java.awt.Color(255, 255, 255));
-        menuEntri.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        menuEntri.setText("Entri");
-        menuEntri.setOpaque(true);
-        menuEntri.setPreferredSize(new java.awt.Dimension(41, 30));
-        menuEntri.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                menuEntriMouseClicked(evt);
+        btnMenuEntri.setBackground(new java.awt.Color(102, 102, 255));
+        btnMenuEntri.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        btnMenuEntri.setForeground(new java.awt.Color(255, 255, 255));
+        btnMenuEntri.setText("Entri");
+        btnMenuEntri.setBorderPainted(false);
+        btnMenuEntri.setOpaque(true);
+        btnMenuEntri.setSize(new java.awt.Dimension(97, 35));
+        btnMenuEntri.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMenuEntriActionPerformed(evt);
             }
         });
 
-        menuValidasi.setBackground(new java.awt.Color(0, 102, 255));
-        menuValidasi.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        menuValidasi.setForeground(new java.awt.Color(255, 255, 255));
-        menuValidasi.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        menuValidasi.setText("Validasi");
-        menuValidasi.setOpaque(true);
-        menuValidasi.setPreferredSize(new java.awt.Dimension(68, 30));
-        menuValidasi.addMouseListener(new java.awt.event.MouseAdapter() {
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
-                menuValidasiMouseClicked(evt);
+        btnMenuValidasi.setBackground(new java.awt.Color(102, 102, 255));
+        btnMenuValidasi.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        btnMenuValidasi.setForeground(new java.awt.Color(255, 255, 255));
+        btnMenuValidasi.setText("Validasi");
+        btnMenuValidasi.setBorderPainted(false);
+        btnMenuValidasi.setOpaque(true);
+        btnMenuValidasi.setSize(new java.awt.Dimension(97, 35));
+        btnMenuValidasi.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMenuValidasiActionPerformed(evt);
             }
         });
 
-        jLabel3.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("Versi 1.0.0");
+        btnMenuProgress.setBackground(new java.awt.Color(102, 102, 255));
+        btnMenuProgress.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        btnMenuProgress.setForeground(new java.awt.Color(255, 255, 255));
+        btnMenuProgress.setText("Progress");
+        btnMenuProgress.setBorderPainted(false);
+        btnMenuProgress.setOpaque(true);
+        btnMenuProgress.setSize(new java.awt.Dimension(97, 35));
+        btnMenuProgress.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMenuProgressActionPerformed(evt);
+            }
+        });
 
-        menuProgress.setBackground(new java.awt.Color(0, 102, 255));
-        menuProgress.setFont(new java.awt.Font("Arial", 1, 18)); // NOI18N
-        menuProgress.setForeground(new java.awt.Color(255, 255, 255));
-        menuProgress.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        menuProgress.setText("Progress");
-        menuProgress.setOpaque(true);
-        menuProgress.setPreferredSize(new java.awt.Dimension(78, 30));
+        btnMenuLogout.setBackground(new java.awt.Color(255, 102, 102));
+        btnMenuLogout.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        btnMenuLogout.setForeground(new java.awt.Color(255, 255, 255));
+        btnMenuLogout.setText("Log Out");
+        btnMenuLogout.setBorderPainted(false);
+        btnMenuLogout.setOpaque(true);
+        btnMenuLogout.setSize(new java.awt.Dimension(97, 35));
+        btnMenuLogout.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMenuLogoutActionPerformed(evt);
+            }
+        });
+
+        btnMenuHome.setBackground(new java.awt.Color(255, 255, 255));
+        btnMenuHome.setFont(new java.awt.Font("Arial", 1, 14)); // NOI18N
+        btnMenuHome.setForeground(new java.awt.Color(255, 255, 255));
+        btnMenuHome.setIcon(new javax.swing.ImageIcon(getClass().getResource("/id/go/bps/lampung/monitorentri/assets/home.png"))); // NOI18N
+        btnMenuHome.setText("Home");
+        btnMenuHome.setAlignmentY(0.0F);
+        btnMenuHome.setBorderPainted(false);
+        btnMenuHome.setBounds(new java.awt.Rectangle(0, 0, 0, 0));
+        btnMenuHome.setContentAreaFilled(false);
+        btnMenuHome.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
+        btnMenuHome.setPreferredSize(new java.awt.Dimension(35, 35));
+        btnMenuHome.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMenuHomeActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout headerPanelLayout = new javax.swing.GroupLayout(headerPanel);
         headerPanel.setLayout(headerPanelLayout);
@@ -110,64 +184,82 @@ public class MainFrame extends javax.swing.JFrame {
             headerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(headerPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(menuEntri, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnMenuHome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(5, 5, 5)
+                .addComponent(btnMenuEntri)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(menuValidasi, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnMenuValidasi)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(menuProgress, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 95, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(btnMenuProgress)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 565, Short.MAX_VALUE)
+                .addComponent(btnMenuLogout, javax.swing.GroupLayout.PREFERRED_SIZE, 116, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
         );
         headerPanelLayout.setVerticalGroup(
             headerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, headerPanelLayout.createSequentialGroup()
-                .addContainerGap(9, Short.MAX_VALUE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addGroup(headerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(menuEntri, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(menuValidasi, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel3)
-                    .addComponent(menuProgress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(15, 15, 15))
+                    .addComponent(btnMenuEntri)
+                    .addComponent(btnMenuValidasi)
+                    .addComponent(btnMenuProgress)
+                    .addComponent(btnMenuLogout)
+                    .addComponent(btnMenuHome, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(13, 13, 13))
         );
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addComponent(mainPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(0, 0, Short.MAX_VALUE))
-            .addComponent(headerPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addComponent(headerPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(mainPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 670, javax.swing.GroupLayout.PREFERRED_SIZE))
-        );
+        getContentPane().add(headerPanel, java.awt.BorderLayout.PAGE_START);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void menuEntriMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuEntriMouseClicked
+    private void btnMenuEntriActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMenuEntriActionPerformed
         // TODO add your handling code here:
-        MainFrame.this.mainPanel.add(new EntrianPanel(this));
-    }//GEN-LAST:event_menuEntriMouseClicked
+        this.mainPanel.removeAll();
+        this.mainPanel.add(new EntrianPanel(this));
+        this.mainPanel.repaint();
+        this.mainPanel.revalidate();
+    }//GEN-LAST:event_btnMenuEntriActionPerformed
 
-    private void menuValidasiMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_menuValidasiMouseClicked
+    private void btnMenuValidasiActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMenuValidasiActionPerformed
         // TODO add your handling code here:
-        MainFrame.this.mainPanel.add(new ValidasiPanel(this));
-    }//GEN-LAST:event_menuValidasiMouseClicked
+        this.mainPanel.removeAll();
+        this.mainPanel.add(new ValidasiPanel(this));
+        this.mainPanel.repaint();
+        this.mainPanel.revalidate();
+    }//GEN-LAST:event_btnMenuValidasiActionPerformed
+
+    private void btnMenuProgressActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMenuProgressActionPerformed
+        // TODO add your handling code here:
+        this.setMenuEnable(true, this.status);
+    }//GEN-LAST:event_btnMenuProgressActionPerformed
+
+    private void btnMenuLogoutActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMenuLogoutActionPerformed
+        // TODO add your handling code here:
+        this.mainPanel.removeAll();
+        this.mainPanel.add(new LoginPanel(this));
+        this.mainPanel.repaint();
+        this.mainPanel.revalidate();
+        this.setMenuEnable(false, 0);
+    }//GEN-LAST:event_btnMenuLogoutActionPerformed
+
+    private void btnMenuHomeActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnMenuHomeActionPerformed
+        // TODO add your handling code here:
+        this.mainPanel.removeAll();
+        this.mainPanel.add(new HomePanel(this));
+        this.mainPanel.repaint();
+        this.mainPanel.revalidate();
+        this.setMenuEnable(true, this.status);
+    }//GEN-LAST:event_btnMenuHomeActionPerformed
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnMenuEntri;
+    private javax.swing.JButton btnMenuHome;
+    private javax.swing.JButton btnMenuLogout;
+    private javax.swing.JButton btnMenuProgress;
+    private javax.swing.JButton btnMenuValidasi;
     private javax.swing.JPanel headerPanel;
-    private javax.swing.JLabel jLabel3;
     private javax.swing.JPanel mainPanel;
-    private javax.swing.JLabel menuEntri;
-    private javax.swing.JLabel menuProgress;
-    private javax.swing.JLabel menuValidasi;
     // End of variables declaration//GEN-END:variables
 }
